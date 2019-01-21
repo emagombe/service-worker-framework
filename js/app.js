586d39d66af63f0ca163d0f990b06b6bab05d7c6
emagombe <edson.patricio.39@gmail.com>
2019-01-21 08:44:24 +0200

    Code format

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
 
 
 if ('serviceWorker' in navigator) {
-	//console.log('serviceWorker is supported!');
 	navigator.serviceWorker.register('sw.js').then(function(registration){
-		//console.log('sw registed');
-		//registration.update();
+		console.log('Service worker registed!!!');
 	}, function(status){
 		console.log('Failed to register:', status);
 	});
--- js/webonfire.js
+++ js/webonfire.js
@@ -1,103 +1,102 @@
-function Webonfire (conf = {
-	appname : "app",
-	version : "1.0.0",
-	mode : "offline-first",
-	cacheAllRequests : true,
-	debug : true
+function Webonfire(conf = {
+  appname: 'app',
+  version: '1.0.0',
+  mode: 'offline-first',
+  cacheAllRequests: true,
+  debug: true
 }) {
-	var root = this;
-
-	this.getAppname = function(){
-		return conf.appname;
-	};
-	this.getVersion = function(){
-		return conf.version;
-	};
-	this.app = function(){
-		return conf.appname + '__v' + conf.version;
-	};
-	this.install = function(resources){
-		self.addEventListener("install", function(event){
-			if(conf.debug){
-				console.log('installing', root.app() + "...");
-			}
-			if(typeof resources != "undefined"){
-				if(typeof resources == "object" && resources != null){
-					event.waitUntil(
-						caches.open(root.app()).then(function(this_cache){
-							return this_cache.addAll(resources);
-						}, function(ex){
-							if (conf.debug) {
-								console.error('Failed to cache files: ', ex);
-							}
-						})
-					);
-				}
-			}
-		});
-		self.addEventListener("fetch", function(event){
-			if(conf.cacheAllRequests === true){
-				event.waitUntil(
-					caches.open(root.app()).then(function(this_cache){
-						return caches.match(event.request).then(function(response){
-							return response || fetch(event.request).then(function(response){
-								this_cache.put(event.request, response.clone());
-							});
-						});
-					})
-				);
-			}
-			if (conf.mode === "offline-first" || conf.mode === "") {
-				event.respondWith(
-					caches.match(event.request).then(function(response){
-						return response || fetch(event.request);
-					}).catch(function(ex){
-						if(conf.debug){
-							console.log('Failed to fetch installed files: ', ex);
-						}
-					})
-				);
-			}
-			if (conf.mode === "online-first") {
-				event.respondWith(fetch(event.request).catch(function(ex){
-					return caches.match(event.request);
-				}));
-			}
-		});
-	};
+  var root = this;
+  this.getAppname = function () {
+    return conf.appname;
+  };
+  this.getVersion = function () {
+    return conf.version;
+  };
+  this.app = function () {
+    return conf.appname + '__v' + conf.version;
+  };
+  this.install = function (resources) {
+    self.addEventListener('install', function (event) {
+      if (conf.debug) {
+        console.log('installing', root.app() + '...');
+      }
+      if (typeof resources != 'undefined') {
+        if (typeof resources == 'object' && resources != null) {
+          event.waitUntil(caches.open(root.app()).then(function (this_cache) {
+            return this_cache.addAll(resources);
+          }, function (ex) {
+            if (conf.debug) {
+              console.error('Failed to cache files: ', ex);
+            }
+          })
+          );
+        }
+      }
+    });
+    self.addEventListener('fetch', function (event) {
+      if (conf.cacheAllRequests === true) {
+        event.waitUntil(caches.open(root.app()).then(function (this_cache) {
+          return caches.match(event.request).then(function (response) {
+            return response || fetch(event.request).then(function (response) {
+              this_cache.put(event.request, response.clone());
+            });
+          });
+        })
+        );
+      }
+      if (conf.mode === 'offline-first' || conf.mode === '') {
+        event.respondWith(caches.match(event.request).then(function (response) {
+          return response || fetch(event.request);
+        }).catch (function (ex) {
+          if (conf.debug) {
+            console.log('Failed to fetch installed files: ', ex);
+          }
+        })
+        );
+      }
+      if (conf.mode === 'online-first') {
+        event.respondWith(fetch(event.request).catch (function (ex) {
+          return caches.match(event.request);
+        }));
+      }
+    });
+  };
 }
-
-function splitApp(app, index = "appname"){
-	details = app.split("__");
-	if(index == "appname"){
-		return details[0];
-	}
-	if(index == "version"){
-		return details[1];
-	}
+function splitApp(app, index = 'appname') {
+  details = app.split('__');
+  if (index == 'appname') {
+    return details[0];
+  }
+  if (index == 'version') {
+    return details[1];
+  }
 }
-
-Webonfire.getVersion = function(appname){
-	return caches.keys().then(function(cacheNames){
-		for (var i = cacheNames.length - 1; i >= 0; i--) {
-			if(appname === splitApp(cacheNames[i], "appname")){
-				return splitApp(cacheNames[i], "version");
-			}
-		}
-	});
+Webonfire.getVersion = function (appname) {
+  return caches.keys().then(function (cacheNames) {
+    for (var i = cacheNames.length - 1; i >= 0; i--) {
+      if (appname === splitApp(cacheNames[i], 'appname')) {
+        return splitApp(cacheNames[i], 'version');
+      }
+    }
+  });
 };
-
-Webonfire.uninstall = function(appname){
-	caches.keys().then(function(cacheNames){
-		for (var i = cacheNames.length - 1; i >= 0; i--) {
-			if (appname === splitApp(cacheNames[i]), "name") { return cacheNames[i]; }
-		}
-		return "none";
-	}).then(function(name){
-		if (name == "none") { if(conf.debug){ console.log("Application not found"); } } 
-		else 
-		{
-			return caches.delete(name);
-		}
-	});
+Webonfire.uninstall = function (appname) {
+  caches.keys().then(function (cacheNames) {
+    for (var i = cacheNames.length - 1; i >= 0; i--) {
+      if (appname === splitApp(cacheNames[i]), 'name') {
+        return cacheNames[i];
+      }
+    }
+    return 'none';
+  }).then(function (name) {
+    if (name == 'none') {
+      if (conf.debug) {
+        console.log('Application not found');
+      }
+    } 
+    else
+    {
+      return caches.delete (name);
+    }
+  });
 };
